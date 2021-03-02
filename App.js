83d050abe337138395c3b8742046ec4fc7d35b54
hiimhung <hungdm.hn@havaz.vn>
2021-03-02 10:24:58 +0700

    okii

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,87 +6,90 @@ import {
   View,
   Text,
   StatusBar,
+  Image
 } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
-import Icon from 'react-native-vector-icons/Entypo';
-import Feat from 'react-native-vector-icons/Feather';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Feat from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
 import Heart from 'react-native-vector-icons/AntDesign';
 import User from 'react-native-vector-icons/FontAwesome';
 import { Router, Scene } from 'react-native-router-flux'
-import Feed from './src/components/Feed';
-import Body from './src/components/Post/Content/Body';
-import Post from './src/components/Post';
-import Stories from './src/components/Stories';
+import HomeScreen from './src/screens/HomeScreen';
+import DiscoveryScreen from './src/screens/DiscoveryScreen';
+import CreatePostScreen from './src/screens/CreatePostScreen';
+import NotificationsScreen from './src/screens/NotificationsScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+
 const App = () => {
   return (
     <>
     <Router>
     <Scene
-          hideNavBar
           showLabel={false}
           tabs>
           <Scene
-              key="product"
+              key="Home"
               component={HomeScreen}
-              hideNavBar
+              renderTitle={({props}) =>
+              <View style={{flexDirection:'row', justifyContent: 'space-between', width:"100%",padding: 5}}>
+                <Image source={require('./images/logo.png')} style={{height: 37, width:'36%'}}/>
+                <Icon name="facebook-messenger" size={30}/>
+              </View> }
               icon={({ focused }) => (
                 <Icon
                   style={{ width: 30 }}
-                  name="home"
+                  name={focused ? "home" : "home-outline"}
                   size={30}
-                  color={focused ? '#000' : '#353535'}
                 />
               )}
             />
             <Scene
-              key="shop"
-              component={Feed}
+              key="Discovery"
+              component={DiscoveryScreen}
               hideNavBar
               icon={({ focused }) => (
                 <Feat
                   style={{ width: 30 }}
-                  name="search"
+                  name={focused ? "search" : "search-outline"}
                   size={30}
-                  color={focused ? '#000' : '#353535'}
                 />
               )}
             />
             <Scene
-              key="user"
-              component={Body}
+              key="Post"
+              component={CreatePostScreen}
               hideNavBar
               icon={({ focused }) => (
-                <Feat
+                <Entypo
                   style={{ width: 30 }}
-                  name="plus-square"
+                  name="squared-plus"
                   size={30}
                   color={focused ? '#000' : '#353535'}
                 />
               )}
             />
             <Scene
-              key="users"
-              component={Post}
+              key="Notifications"
+              component={NotificationsScreen}
               hideNavBar
               icon={({ focused }) => (
                 <Heart
                   style={{ width: 30 }}
                   name="hearto"
+                  name={focused ? "heart" : "hearto"}
                   size={30}
-                  color={focused ? '#000' : '#353535'}
                 />
               )}
             />
             <Scene
-              key="userse"
-              component={Stories}
+              key="Profile"
+              component={ProfileScreen}
               hideNavBar
               icon={({ focused }) => (
                 <User
                   style={{ width: 30 }}
-                  name="user-circle-o"
+                  name={focused ? "user-circle" : "user-circle-o"}
                   size={30}
-                  color={focused ? '#000' : '#353535'}
                 />
               )}
             />
---
+++ src/screens/NotificationsScreen/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Feed from '../../components/Feed';
+
+const NotificationsScreen = () => {
+    return (
+        <View style={styles.container}>
+            <Text>
+                dsfsafsdf
+            </Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'red'
+    }
+});
+
+
+export default NotificationsScreen
